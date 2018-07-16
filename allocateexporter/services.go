13fa3a5541c74d4984ffd3da0290f0a0c04c1488
Nigel Magnay <nigel.magnay@gmail.com>
2018-07-16 14:25:20 +0100

    work

---
+++ allocateexporter/services.go
@@ -0,0 +1,12 @@
+package allocateexporter
+
+import (
+	"fmt"	
+)
+
+
+func Find_services() {
+    // Display count of apples and oranges.
+    fmt.Println("apples")    
+    fmt.Println("oranges")    
+}
--- json_exporter.go
+++ json_exporter.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"github.com/kawamuray/prometheus-exporter-harness/harness"
-	"github.com/kawamuray/prometheus-json-exporter/jsonexporter"
+	"github.com/AllocateSoftware/prometheus-allocate-exporter/jsonexporter"
+	"github.com/AllocateSoftware/prometheus-allocate-exporter/allocateexporter"
 )
 
 func main() {
+
+	fmt.Println("Start me up")
+
+	allocateexporter.Find_services
+
+	fmt.Println("Started me up")
+
+
 	opts := harness.NewExporterOpts("json_exporter", jsonexporter.Version)
 	opts.Usage = "[OPTIONS] HTTP_ENDPOINT CONFIG_PATH"
 	opts.Init = jsonexporter.Init
